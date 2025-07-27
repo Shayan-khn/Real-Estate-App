7d81d4cdc9389ee704dd940fdacdaed767e4fdcd
Shayan <shayanhamid031@gmail.com>
2025-07-27 17:12:53 +0500

    Connected to MongoDB

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to MongoDB")
+}).catch((err)=>{
+    console.log("Error:",err)
+})
 const app = express()
 
-app.listen(3500,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("App Running at port 3500!")
 })
