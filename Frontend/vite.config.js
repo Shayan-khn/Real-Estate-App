8b46f909b53418037d693991061061caeafb8ed8
Shayan <shayanhamid031@gmail.com>
2025-07-30 23:19:10 +0500

    Complete Signup Functionality Added

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  server: {
+    proxy:{
+      '/core-api-auth':{
+        target: 'http://localhost:3500',
+        secure: false,
+      }
+    }
+  },
   plugins: [
     tailwindcss(),
   ],
